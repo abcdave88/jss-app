8445155a8c3f60662eff4f511abb85fc3ad22542
David James <d.james88@btinternet.com>
2015-07-29 14:49:19 +0100

    partials utilized for about page

--- server.js
+++ server.js
@@ -18,7 +18,11 @@ app.use(express.static(__dirname + '/public'));
 
 
 app.get('/', function(req, res){
-  res.render('index');
+  res.render('pages/index');
+});
+
+app.get('/about', function(req, res){
+  res.render('pages/about');
 });
 
 server.listen(port, function(){
