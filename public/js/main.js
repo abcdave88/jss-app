d38689b775a239858f801856b48a966a0b258c58
David James <d.james88@btinternet.com>
2015-08-05 14:32:36 +0100

    fade in & out of jumbo image working.

--- public/js/main.js
+++ public/js/main.js
@@ -1,29 +1,49 @@
-console.log('hello world')
+// console.log('hello world')
 
 $(document).ready(function(){
   //works
-  var header = $('.jumbo1 #contain-h');
-  var image = $('.jumbo1 #jumbo-img');
-  var imageUrls = ["http://arpadlukacs.com/wp-content/uploads/2012/09/975.-London-Skyline-from-Gypsy-Hill.jpg", "https://upload.wikimedia.org/wikipedia/commons/f/ff/Cabot_Square,_Canary_Wharf_-_June_2008.jpg", "http://www.gocre8.co.uk/wp-content/uploads/2015/03/NHS-Building-site-banners.png"];
-  var imageHeadings = ['Something About Procurement', 'Something About Money And How We Can Save You A Shit Tonne', 'Something About Owning The Shit Out Of Public & Private Sectors'];
-  var count = 0;
+//   var header = $('.jumbo1 .contain-h');
+//   var image = $('.jumbo1 .jumbo-img');
+//   var imageUrls = ["http://arpadlukacs.com/wp-content/uploads/2012/09/975.-London-Skyline-from-Gypsy-Hill.jpg", "https://upload.wikimedia.org/wikipedia/commons/f/ff/Cabot_Square,_Canary_Wharf_-_June_2008.jpg", "http://www.gocre8.co.uk/wp-content/uploads/2015/03/NHS-Building-site-banners.png"];
+//   var imageHeadings = ['Something About Procurement', 'Something About Money And How We Can Save You A Shit Tonne', 'Something About Owning The Shit Out Of Public & Private Sectors'];
+//   var count = 0;
 
-  function changeJumbo(){
-    image.attr('src', imageUrls[count])
-    header.html(imageHeadings[count])
+//   function changeJumbo(){
+//     image.attr('src', imageUrls[count])
+//     header.html(imageHeadings[count])
 
-    count ++
-    console.log('jumbo changed')
-    setTimeout(changeJumbo, 5000)
-     if (count === imageUrls.length){
-      count = 0
-     }
-  };
+//     count ++
+//     console.log('jumbo changed')
+//     setTimeout(changeJumbo, 5000)
+//      if (count === imageUrls.length){
+//       count = 0
+//      }
+//   };
 
-changeJumbo()
+// changeJumbo()
 //works
 
+//not working
+// $('#jumbotron1').hide();
+$('#jumbotron2').hide();
+$('#jumbotron3').hide();
 
-
+function changeJumbo(){
+ 
+  $('#jumbotron1').delay(3000).fadeOut(1500, function(){
+      $('#jumbotron2').fadeIn(1500).delay(3000).fadeOut(1500, function(){
+        $('#jumbotron3').fadeIn(1500).delay(3000).fadeOut(1500, function(){
+          $('#jumbotron1').fadeIn(1500, function(){
+              var i = 0;
+              if (i < 1){
+                changeJumbo();
+              }
+          });
+        });
+      });
+  });
+}
+changeJumbo();
+//not working
 
 });//end of document.ready
